23bcda0a78206b1f373b05d83fa64d90b4647d13
Заид Мингалиев <zzmingaliev@outlook.com>
2021-11-02 17:58:05 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PuteshestviePoRossii4433
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TESTDBSecondEntities _context = new TESTDBSecondEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListTours.ItemsSource = _context.Tour.OrderBy(tour => tour.Name).ToList();
+        }
+
+        private void TxtFindedTourName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ChbActual_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChbActual_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
